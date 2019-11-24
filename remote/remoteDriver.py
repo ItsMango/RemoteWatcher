c026b1b63e5e67b1f2b8a51ef122ba480de4be10
Edward Jing <jingyuan0014@foxmail.com>
2019-11-24 17:24:43 +0800

    add test dir

--- remote/remoteDriver.py
+++ remote/remoteDriver.py
@@ -1,5 +1,8 @@
 import cv2
 
 img = cv2.imread("/home/pi/Pictures/1.jpg", cv2.IMREAD_COLOR)
+width, heigh = img.shape[0: 2]
+print("Width: %d, %d" % (width, heigh))
+img = cv2.resize(img, (int(heigh/4), int(width/4)))
 cv2.imshow("img", img)
-cv2.waitKey(1)
+cv2.waitKey(0)
